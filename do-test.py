82c5858d98b2906275efd5ccf20edc561f69207d
Vyshak <vyshak.m@coviam.com>
2020-05-20 19:59:24 +0530

    string sub 2

--- do-test.py
+++ do-test.py
@@ -29,7 +29,7 @@ except IndexError:
 
 
 endpoint = "https://api.digitalocean.com/v2/firewalls/"
-headers = {"Authorization": "Bearer %s".format(TOKEN)}
+headers = {"Authorization": "Bearer %s".format(token)}
 response = requests.get(endpoint, headers=headers).json()
 firewalls = response["firewalls"]
 firewall_ids = []
